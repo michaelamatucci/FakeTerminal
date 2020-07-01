6c4aae839002eff252af508a8fc383bdbc3cda28
michael <michaelamatucci17@gmail.com>
2020-06-30 20:48:42 -0400

    Did things with drive names

--- FakeTerminal.cpp
+++ FakeTerminal.cpp
@@ -1,11 +1,26 @@
 
 #include <iostream>
+#include <string>
+#include <locale>
 #include <chrono>
 #include <thread>
 using namespace std;
 using namespace this_thread;
 using namespace chrono_literals;
 
+/*
+This program literally only simulates a fake installation
+of a fake OS
+
+Just for fun :)
+
+*/
+
+/*
+Makes the dot loading indicators
+int amount -> the amount of dots to display (how long)
+int time -> selects the speed of the dots
+*/
 void dotLoading(int amount, int time)
 {
     if (time == 1)
@@ -42,16 +57,23 @@ void dotLoading(int amount, int time)
 
 int main(int argc, char *argv[])
 {
-    string name;
+    string name; //holder for drive name
+
+    //outputs the fake terminal
 
     sleep_for(500ms);
     cout << "256kb RAM OK\n\n";
     sleep_for(1s);
     cout << "80mb Drive Detected\n\n";
     sleep_for(1s);
+    cout << "Insert boot disk and press ENTER\n";
+    cin.ignore();
+    sleep_for(1s);
+    cout << "FUTUREOS SETUP\n";
+    sleep_for(250ms);
     cout << "Enter the name of the drive to install on: ";
     cin >> name;
-    cout << "\nStarting FutureOS Installation";
+    cout << "\nStarting FutureOS Installation on drive " + name + ":";
     dotLoading(3, 1);
     cout << "\n\nInstalling Kernal v1.5.34";
     dotLoading(5, 1);
@@ -75,6 +97,6 @@ int main(int argc, char *argv[])
     cout << "Setup Complete\n\n";
     sleep_for(500ms);
     cout << "Welcome to FUTUREOS\n";
-    cout << "\n\nC:\\>_";
+    cout << "\n\n" + name + ":\\>_";
 }
 
